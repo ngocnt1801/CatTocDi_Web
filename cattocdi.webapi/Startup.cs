1f752e1fc7a497672ce5f033dc3b5f6839c7cf8e
tiendat <tiendatdeveloper1997@gmail.com>
2018-09-24 22:18:48 +0700

    Create Base Projects

---
+++ cattocdi.webapi/Startup.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartup(typeof(cattocdi.webapi.Startup))]
+
+namespace cattocdi.webapi
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
