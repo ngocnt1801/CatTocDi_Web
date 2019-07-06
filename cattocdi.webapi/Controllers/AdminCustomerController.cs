64435c0843b2c730c6db5380762cc190ff71abd9
ngocnt1801 <ngocnt1801@gmail.com>
2019-07-06 21:04:46 +0700

    NgocNT_Get salon list, salon appointment,salon customer

---
+++ cattocdi.webapi/Controllers/AdminCustomerController.cs
@@ -0,0 +1,37 @@
+﻿using cattocdi.salonservice.Interface;
+using Elmah;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace cattocdi.webapi.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/admin/salon/customer")]
+    public class AdminCustomerController : ApiController
+    {
+        private ICustomerService _customerService;
+        public AdminCustomerController(ICustomerService customerService)
+        {
+            _customerService = customerService;
+        }
+
+        //GET : api/Customer
+        public IHttpActionResult Get(int salonId)
+        {
+            try
+            {
+                var customers = _customerService.GetAllSalonCustomerForAdmin(salonId);
+                return Json(customers);
+            }
+            catch (Exception ex)
+            {
+                ErrorSignal.FromCurrentContext().Raise(ex);
+                return BadRequest("Get Customer Failed");
+            }
+        }
+    }
+}
---
+++ cattocdi.webapi/Controllers/AdminSalonController.cs
@@ -0,0 +1,51 @@
+﻿using cattocdi.salonservice.Interface;
+using Elmah;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net;
+using System.Net.Http;
+using System.Web.Http;
+
+namespace cattocdi.webapi.Controllers
+{
+    [AllowAnonymous]
+    [RoutePrefix("api/admin/salon")]
+    public class AdminSalonController : ApiController
+    {
+        private ISalonServices _salonService;
+
+        public AdminSalonController(ISalonServices salonServices)
+        {
+            _salonService = salonServices;
+        }
+
+        public IHttpActionResult Get()
+        {
+            try
+            {
+                var model = _salonService.GetSalonsForAdmin();
+                return Json(model);
+            }
+            catch (Exception ex)
+            {
+                ErrorSignal.FromCurrentContext().Raise(ex);
+                return BadRequest("Get Statistic Failed");
+            }
+        }
+
+        public IHttpActionResult Get(int salonId, int month)
+        {
+            try
+            {
+                var model = _salonService.GetAppointmentsByMonth(salonId, month);
+                return Json(model);
+            }
+            catch (Exception ex)
+            {
+                ErrorSignal.FromCurrentContext().Raise(ex);
+                return BadRequest("Get Statistic Failed");
+            }
+        }
+    }
+}
